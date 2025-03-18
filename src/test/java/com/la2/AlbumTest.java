702df9cafed9e7aa5f785bec0f0bcdcac4170d25
varphi-online <nbash1984@gmail.com>
2025-03-18 12:52:42 -0700

    merge experimental with maven

--- src/test/java/com/la2/AlbumTest.java
+++ src/test/java/com/la2/AlbumTest.java
@@ -1,15 +1,14 @@
-package test.model;
+package com.la2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
 
-import main.model.Album;
 
 @Testable
 class AlbumTest {
-	private Album foundationAlbum = new Album("Dark Side of the Moon", "Pink Floyd", "Psychedelic Rock", 1973);
+	private Album foundationAlbum = new Album("Dark Side of the Moon", "Pink Floyd", "Psychedelic Rock", 1973);)
 
 	@Test
 	void printTest() {
